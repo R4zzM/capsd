0cd48188f19eb1fdf81f07950255bdf8a9cfe517
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2014-04-17 08:23:58 +0200

    Added select(...) debug functionality.

--- src/readkbd.c
+++ src/readkbd.c
@@ -8,23 +8,37 @@ int main(int argc, char** argv)
   struct input_event ev;  
   int fd;
   int nbytes;
+  fd_set readfds;
+  int ret;
   
-  const char *file = "/dev/input/event5";
-  fd = fopen(file, O_RDONLY);
+  const char *file = "/dev/input/event18";
+  fd = open(file, O_RDONLY);
   if(fd == -1) {
     printf("Could not open %s\n", file);
     goto err;
   }
+
+  FD_ZERO(&readfds);
+  FD_SET(fd, &readfds);
   
   while(1) {
-    nbytes = read(fd, &ev, sizeof(ev));
-    if(nbytes != sizeof(ev)) {
-      // Not sure if this is possible.
-      printf("Crap. Got %d bytes but expected %u.\n", nbytes, sizeof(ev));
-      goto err_close_file;
+    ret = select(fd + 1, &readfds, NULL, NULL, NULL);
+    if(ret == -1) {
+      printf("select(...) returned with error.");
+      continue;
     } else {
-      printf("ev.type = %u, ev.code = %u, ev.value = %u\n", ev.type, ev.code, 
-          ev.value);
+      printf("select returned...");
+    }
+    if(FD_ISSET(fd, &readfds)) {
+      nbytes = read(fd, &ev, sizeof(ev));
+      if(nbytes != sizeof(ev)) {
+        // Not sure if this is possible.
+        printf("Crap. Got %d bytes but expected %u.\n", nbytes, sizeof(ev));
+        goto err_close_file;
+      } else {
+        printf("ev.type = %u, ev.code = %u, ev.value = %u\n", ev.type, ev.code, 
+            ev.value);
+      }
     }
   }
   
